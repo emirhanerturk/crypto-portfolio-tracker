2dfd3e86be519db8a7ca5c731b184c2a4c8712aa
Emirhan Erturk <emirhan@rapsodo.com>
2021-03-10 23:47:46 +0300

    created layout and brand component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrandComponent } from './components/brand/brand.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandComponent
   ],
   imports: [
     BrowserModule
