ccf0e7599b4d5a1db6ac5911f359f810701656da
Emirhan Erturk <emirhan@rapsodo.com>
2021-03-21 14:46:20 +0300

    updated env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  /**
+   * IMPORTANT!
+   * Defining your API key here creates a security risk.
+   * In a project that you will publish in a public, you must make these requests on the backend side.
+   */
+  coinMarketCap: {
+    host: 'https://pro-api.coinmarketcap.com',
+    version: 'v1',
+    apiKey: 'XXXX-XXXX-XXXX'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  /**
+   * IMPORTANT!
+   * Defining your API key here creates a security risk.
+   * In a project that you will publish in a public, you must make these requests on the backend side.
+   */
+  coinMarketCap: {
+    host: 'https://pro-api.coinmarketcap.com',
+    version: 'v1',
+    apiKey: 'XXXX-XXXX-XXXX'
+  }
 };
 
 /*
